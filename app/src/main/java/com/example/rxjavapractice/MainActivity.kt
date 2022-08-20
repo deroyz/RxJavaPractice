765576f0f3b986b7d0d079865c87464ce3135566
Young <deroyz@hotmail.com>
2022-08-20 16:10:30 -0600

    RxJava - Operators

--- app/src/main/java/com/example/rxjavapractice/MainActivity.kt
+++ app/src/main/java/com/example/rxjavapractice/MainActivity.kt
@@ -12,8 +12,6 @@ import android.view.Menu
 import android.view.MenuItem
 import com.example.rxjavapractice.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,52 +38,197 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-//        justOperator()
-//        fromOperator()
-//        fromIterableOperator()
-//
-//        rangeOperator().subscribe({
-//            Log.d(MainActivity.TAG, "onNext: $it")
-//        }, {
-//            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
-//        }, {
-//            Log.d(MainActivity.TAG, "onComplete")
-//        })
-//
-//        repeatOperator().subscribe({
-//            Log.d(MainActivity.TAG, "onNext: $it")
-//        }, {
-//            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
-//        }, {
-//            Log.d(MainActivity.TAG, "onComplete")
-//        })
-//
-//        intervalOperator().subscribe({
-//            Log.d(MainActivity.TAG, "onNext: $it")
-//            callYoung()
-//        }, {
-//            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
-//        }, {
-//            Log.d(MainActivity.TAG, "onComplete")
-//        })
-//
-//        timerOperator().subscribe({
-//            Log.d(MainActivity.TAG, "onNext: $it")
-//            callYoung()
-//        }, {
-//            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
-//        }, {
-//            Log.d(MainActivity.TAG, "onComplete")
-//        })
+        /*justOperator()*/
 
-        createOperator().subscribe({
+        /*fromOperator()*/
+
+        /*fromIterableOperator()*/
+
+        /*rangeOperator().subscribe({
+            Log.d(MainActivity.TAG, "onNext: $it")
+        }, {
+            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+        }, {
+            Log.d(MainActivity.TAG, "onComplete")
+        })*/
+
+        /*repeatOperator().subscribe({
+            Log.d(MainActivity.TAG, "onNext: $it")
+        }, {
+            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+        }, {
+            Log.d(MainActivity.TAG, "onComplete")
+        })*/
+
+        /*intervalOperator().subscribe({
             Log.d(MainActivity.TAG, "onNext: $it")
             callYoung()
         }, {
             Log.d(MainActivity.TAG, "onError: ${it.toString()}")
         }, {
             Log.d(MainActivity.TAG, "onComplete")
+        })*/
+
+        /*timerOperator().subscribe({
+            Log.d(MainActivity.TAG, "onNext: $it")
+            callYoung()
+        }, {
+            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+        }, {
+            Log.d(MainActivity.TAG, "onComplete")
+        })*/
+
+        /* createOperator().subscribe({
+             Log.d(MainActivity.TAG, "onNext: $it")
+         }, {
+             Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+         }, {
+             Log.d(MainActivity.TAG, "onComplete")
+         })*/
+
+        /*filterOperator()
+            .filter {
+                it.age > 10
+            }
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            }, {
+                Log.d(MainActivity.TAG, "onComplete")
+            })*/
+
+        /*lastOperator()
+//            .last(User(1, "demo1", 12))
+            .lastElement()
+             .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            })*/
+
+        /*lastOperatorEmptyList()
+//            .last(User(1, "demo1", 12))
+//            .lastOrError()
+            .lastElement()
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            })*/
+
+        /*distinctOperator()
+            .distinct{
+            it.age
+        }
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            }, {
+                Log.d(MainActivity.TAG, "onComplete")
+            })*/
+
+        /*skipOperator()
+//            .skip(2)
+//            .distinct{
+//                it.age
+//            }
+//            .skipLast(2)
+            .skip(1, TimeUnit.MILLISECONDS)
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            }, {
+                Log.d(MainActivity.TAG, "onComplete")
+            })*/
+
+        /*skipOperator()
+//            .skip(2)
+//            .distinct{
+//                it.age
+//            }
+//            .skipLast(2)
+    .skip(1, TimeUnit.MILLISECONDS)
+    .subscribe({
+        Log.d(MainActivity.TAG, "onNext: $it")
+    }, {
+        Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+    }, {
+        Log.d(MainActivity.TAG, "onComplete")
+    })*/
+
+        /*bufferOperator()
+            .buffer(3)
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            }, {
+                Log.d(MainActivity.TAG, "onComplete")
+            })*/
+
+        /*mapOperator()
+//            .map { it.age * 2 }
+            .map{
+                UserProfile(id = it.id, name = it.name, it.age, "https://test.com/${it.id}")
+            }
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            }, {
+                Log.d(MainActivity.TAG, "onComplete")
+            })*/
+
+        /*flatMapOperator()
+            .flatMap {
+                getUserProfile(it.id)
+            }
+            .subscribe({
+                Log.d(MainActivity.TAG, "onNext: $it")
+            }, {
+                Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+            }, {
+                Log.d(MainActivity.TAG, "onComplete")
+            })*/
+
+        /*flatMapOperatorT2()
+            .flatMap{
+                Observable.fromIterable(it)
+            }
+            .flatMap{
+                getUserProfile(it.id)
+            }
+//            .map{
+//                getUserProfile(it.id)
+//            }
+            .subscribe({
+            Log.d(MainActivity.TAG, "onNext: $it")
+        }, {
+            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+        }, {
+            Log.d(MainActivity.TAG, "onComplete")
+        })*/
+
+        groupByOperator()
+            .groupBy {
+                it.age
+            }
+            .subscribe({ group ->
+                       group.subscribe({
+                           Log.d(MainActivity.TAG, "onNext: $it")
+                       },{
+                           Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+                       })
+//            Log.d(MainActivity.TAG, "onNext: $it")
+        }, {
+            Log.d(MainActivity.TAG, "onError: ${it.toString()}")
+        }, {
+            Log.d(MainActivity.TAG, "onComplete")
         })
+
     }
 
 
--- app/src/main/java/com/example/rxjavapractice/Operators.kt
+++ app/src/main/java/com/example/rxjavapractice/Operators.kt
@@ -2,6 +2,7 @@ package com.example.rxjavapractice
 
 import android.util.Log
 import com.example.rxjavapractice.data.User
+import com.example.rxjavapractice.data.UserProfile
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.core.Observer
@@ -13,31 +14,34 @@ import java.util.concurrent.TimeUnit
 val mNumList = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 val arraysNum1 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 val arraysNum2 = arrayOf(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120)
-val userList = mutableListOf<User>(
+val mUserList = mutableListOf<User>(
     User(1, "demo1", 12),
     User(2, "demo2", 15),
-    User(3, "demo3", 16)
+    User(3, "demo3", 23),
+    User(4, "demo4", 12),
+    User(5, "demo5", 23),
+    User(6, "demo6", 16),
+    User(7, "demo7", 23),
+    User(8, "demo8", 16),
+    User(9, "demo9", 16)
 )
+val mUserProfileList = mutableListOf<UserProfile>(
+    UserProfile(1, "demo1", 12, "https://test.com/1"),
+    UserProfile(2, "demo2", 15, "https://test.com/2"),
+    UserProfile(3, "demo3", 23, "https://test.com/3"),
+    UserProfile(4, "demo4", 12, "https://test.com/4"),
+    UserProfile(5, "demo5", 23, "https://test.com/5"),
+    UserProfile(6, "demo6", 16, "https://test.com/6"),
+    UserProfile(7, "demo7", 23, "https://test.com/7"),
+    UserProfile(8, "demo8", 16, "https://test.com/8"),
+    UserProfile(9, "demo9", 16, "https://test.com/9"),
+)
+val mEmptyUserList = mutableListOf<User>()
 
 
 fun justOperator() {
     val observable = Observable.just(mNumList, mNumList)
     val observer = object : Observer<List<Int>> {
-        //            override fun onSubscribe(d: Disposable) {
-//                Log.d(TAG, "onSubscribe")
-//            }
-//
-//            override fun onNext(t: Int) {
-//                Log.d(TAG, "onNext $t")
-//            }
-//
-//            override fun onError(e: Throwable) {
-//                Log.d(TAG, "onError ${e.toString()}")
-//            }
-//
-//            override fun onComplete() {
-//                Log.d(TAG, "onComplete")
-//            }
         override fun onSubscribe(d: Disposable) {
             Log.d(MainActivity.TAG, "onSubscribe")
         }
@@ -137,5 +141,52 @@ fun createOperator(): Observable<Int> {
             it.onError(e)
         }
     })
+}
+
+fun filterOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun lastOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun lastOperatorEmptyList(): Observable<User> {
+    return Observable.fromIterable(mEmptyUserList)
+}
+
+fun distinctOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun skipOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun bufferOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun mapOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun flatMapOperator(): Observable<User> {
+    return Observable.fromIterable(mUserList)
+}
+
+fun getUserProfile(id: Long): Observable<UserProfile> {
+    return Observable.fromIterable(mUserProfileList)
+        .filter {
+            it.id == id
+        }
+}
+
+fun flatMapOperatorT2(): Observable<List<User>>{
+    return Observable.just(mUserList)
+}
+
+fun groupByOperator(): Observable<User>{
+    return Observable.fromIterable(mUserList)
 }
 
